73332108701de9ce392018353a53f3deec337b69
sm09 <sm09@DESKTOP-6A08AK4>
2020-03-16 12:32:50 +0900

    지도추가

---
+++ secondproject/WebContent/mapdata.js
@@ -0,0 +1,849 @@
+var simplemaps_worldmap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    popups: "detect",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "yes",
+    all_states_zoomable: "no",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "http://localhost:8081/secondproject/main2.html",
+    location_size: "20",
+    location_type: "marker",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "yes",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: "",
+    manual_zoom: ""
+  },
+  state_specific: {
+    "0": {},
+    AF: {
+      name: "Afghanistan"
+    },
+    AO: {
+      name: "Angola"
+    },
+    AL: {
+      name: "Albania"
+    },
+    AE: {
+      name: "United Arab Emirates",
+      color: "#ff0067"
+    },
+    AR: {
+      name: "Argentina"
+    },
+    AM: {
+      name: "Armenia"
+    },
+    AU: {
+      name: "Australia",
+      color: "#ff0067"
+    },
+    AT: {
+      name: "Austria"
+    },
+    AZ: {
+      name: "Azerbaijan"
+    },
+    BI: {
+      name: "Burundi"
+    },
+    BE: {
+      name: "Belgium"
+    },
+    BJ: {
+      name: "Benin"
+    },
+    BF: {
+      name: "Burkina Faso"
+    },
+    BD: {
+      name: "Bangladesh"
+    },
+    BG: {
+      name: "Bulgaria"
+    },
+    BH: {
+      name: "Bahrain"
+    },
+    BA: {
+      name: "Bosnia and Herzegovina"
+    },
+    BY: {
+      name: "Belarus"
+    },
+    BZ: {
+      name: "Belize"
+    },
+    BO: {
+      name: "Bolivia"
+    },
+    BR: {
+      name: "Brazil",
+      color: "#ff0067"
+    },
+    BN: {
+      name: "Brunei Darussalam"
+    },
+    BT: {
+      name: "Bhutan"
+    },
+    BW: {
+      name: "Botswana"
+    },
+    CF: {
+      name: "Central African Republic"
+    },
+    CA: {
+      name: "Canada",
+      color: "#ff0067"
+    },
+    CH: {
+      name: "Switzerland"
+    },
+    CL: {
+      name: "Chile"
+    },
+    CN: {
+      name: "China"
+    },
+    CI: {
+      name: "Côte d'Ivoire"
+    },
+    CM: {
+      name: "Cameroon"
+    },
+    CD: {
+      name: "Democratic Republic of the Congo"
+    },
+    CG: {
+      name: "Republic of Congo"
+    },
+    CO: {
+      name: "Colombia"
+    },
+    CR: {
+      name: "Costa Rica"
+    },
+    CU: {
+      name: "Cuba"
+    },
+    CZ: {
+      name: "Czech Republic"
+    },
+    DE: {
+      name: "Germany",
+      color: "#ff0067"
+    },
+    DJ: {
+      name: "Djibouti"
+    },
+    DK: {
+      name: "Denmark"
+    },
+    DO: {
+      name: "Dominican Republic"
+    },
+    DZ: {
+      name: "Algeria"
+    },
+    EC: {
+      name: "Ecuador"
+    },
+    EG: {
+      name: "Egypt"
+    },
+    ER: {
+      name: "Eritrea"
+    },
+    EE: {
+      name: "Estonia"
+    },
+    ET: {
+      name: "Ethiopia"
+    },
+    FI: {
+      name: "Finland",
+      color: "#ff0067"
+    },
+    FJ: {
+      name: "Fiji"
+    },
+    GA: {
+      name: "Gabon"
+    },
+    GB: {
+      name: "United Kingdom",
+      color: "#ff0067"
+    },
+    GE: {
+      name: "Georgia"
+    },
+    GH: {
+      name: "Ghana"
+    },
+    GN: {
+      name: "Guinea"
+    },
+    GM: {
+      name: "The Gambia"
+    },
+    GW: {
+      name: "Guinea-Bissau"
+    },
+    GQ: {
+      name: "Equatorial Guinea"
+    },
+    GR: {
+      name: "Greece"
+    },
+    GL: {
+      name: "Greenland"
+    },
+    GT: {
+      name: "Guatemala"
+    },
+    GY: {
+      name: "Guyana"
+    },
+    HN: {
+      name: "Honduras"
+    },
+    HR: {
+      name: "Croatia"
+    },
+    HT: {
+      name: "Haiti"
+    },
+    HU: {
+      name: "Hungary"
+    },
+    ID: {
+      name: "Indonesia",
+      color: "#ff0067"
+    },
+    IN: {
+      name: "India",
+      color: "#ff0067"
+    },
+    IE: {
+      name: "Ireland"
+    },
+    IR: {
+      name: "Iran"
+    },
+    IQ: {
+      name: "Iraq"
+    },
+    IS: {
+      name: "Iceland"
+    },
+    IL: {
+      name: "Israel"
+    },
+    IT: {
+      name: "Italy"
+    },
+    JM: {
+      name: "Jamaica"
+    },
+    JO: {
+      name: "Jordan"
+    },
+    JP: {
+      name: "Japan",
+      color: "#ff0067"
+    },
+    KZ: {
+      name: "Kazakhstan"
+    },
+    KE: {
+      name: "Kenya",
+      color: "#ff0067"
+    },
+    KG: {
+      name: "Kyrgyzstan"
+    },
+    KH: {
+      name: "Cambodia"
+    },
+    KR: {
+      name: "Republic of Korea"
+    },
+    XK: {
+      name: "Kosovo"
+    },
+    KW: {
+      name: "Kuwait"
+    },
+    LA: {
+      name: "Lao PDR"
+    },
+    LB: {
+      name: "Lebanon"
+    },
+    LR: {
+      name: "Liberia"
+    },
+    LY: {
+      name: "Libya"
+    },
+    LK: {
+      name: "Sri Lanka"
+    },
+    LS: {
+      name: "Lesotho"
+    },
+    LT: {
+      name: "Lithuania"
+    },
+    LU: {
+      name: "Luxembourg"
+    },
+    LV: {
+      name: "Latvia"
+    },
+    MA: {
+      name: "Morocco"
+    },
+    MD: {
+      name: "Moldova"
+    },
+    MG: {
+      name: "Madagascar"
+    },
+    MX: {
+      name: "Mexico",
+      color: "#ff0067"
+    },
+    MK: {
+      name: "Macedonia"
+    },
+    ML: {
+      name: "Mali"
+    },
+    MM: {
+      name: "Myanmar"
+    },
+    ME: {
+      name: "Montenegro"
+    },
+    MN: {
+      name: "Mongolia"
+    },
+    MZ: {
+      name: "Mozambique"
+    },
+    MR: {
+      name: "Mauritania"
+    },
+    MW: {
+      name: "Malawi"
+    },
+    MY: {
+      name: "Malaysia",
+      color: "#ff0067"
+    },
+    NA: {
+      name: "Namibia"
+    },
+    NE: {
+      name: "Niger"
+    },
+    NG: {
+      name: "Nigeria"
+    },
+    NI: {
+      name: "Nicaragua"
+    },
+    NL: {
+      name: "Netherlands"
+    },
+    NO: {
+      name: "Norway"
+    },
+    NP: {
+      name: "Nepal"
+    },
+    NZ: {
+      name: "New Zealand",
+      color: "#ff0067"
+    },
+    OM: {
+      name: "Oman"
+    },
+    PK: {
+      name: "Pakistan",
+      color: "#ff0067"
+    },
+    PA: {
+      name: "Panama"
+    },
+    PE: {
+      name: "Peru"
+    },
+    PH: {
+      name: "Philippines"
+    },
+    PG: {
+      name: "Papua New Guinea"
+    },
+    PL: {
+      name: "Poland"
+    },
+    KP: {
+      name: "Dem. Rep. Korea"
+    },
+    PT: {
+      name: "Portugal"
+    },
+    PY: {
+      name: "Paraguay"
+    },
+    PS: {
+      name: "Palestine"
+    },
+    QA: {
+      name: "Qatar"
+    },
+    RO: {
+      name: "Romania"
+    },
+    RU: {
+      name: "Russia",
+      color: "#ff0067"
+    },
+    RW: {
+      name: "Rwanda"
+    },
+    EH: {
+      name: "Western Sahara"
+    },
+    SA: {
+      name: "Saudi Arabia"
+    },
+    SD: {
+      name: "Sudan"
+    },
+    SS: {
+      name: "South Sudan"
+    },
+    SN: {
+      name: "Senegal"
+    },
+    SL: {
+      name: "Sierra Leone"
+    },
+    SV: {
+      name: "El Salvador"
+    },
+    RS: {
+      name: "Serbia"
+    },
+    SR: {
+      name: "Suriname"
+    },
+    SK: {
+      name: "Slovakia"
+    },
+    SI: {
+      name: "Slovenia"
+    },
+    SE: {
+      name: "Sweden"
+    },
+    SZ: {
+      name: "Swaziland"
+    },
+    SY: {
+      name: "Syria"
+    },
+    TD: {
+      name: "Chad"
+    },
+    TG: {
+      name: "Togo"
+    },
+    TH: {
+      name: "Thailand"
+    },
+    TJ: {
+      name: "Tajikistan"
+    },
+    TM: {
+      name: "Turkmenistan"
+    },
+    TL: {
+      name: "Timor-Leste"
+    },
+    TN: {
+      name: "Tunisia"
+    },
+    TR: {
+      name: "Turkey"
+    },
+    TW: {
+      name: "Taiwan"
+    },
+    TZ: {
+      name: "Tanzania"
+    },
+    UG: {
+      name: "Uganda"
+    },
+    UA: {
+      name: "Ukraine"
+    },
+    UY: {
+      name: "Uruguay"
+    },
+    US: {
+      name: "United States",
+      color: "#ff0067"
+    },
+    UZ: {
+      name: "Uzbekistan"
+    },
+    VE: {
+      name: "Venezuela"
+    },
+    VN: {
+      name: "Vietnam"
+    },
+    VU: {
+      name: "Vanuatu"
+    },
+    YE: {
+      name: "Yemen"
+    },
+    ZA: {
+      name: "South Africa",
+      color: "#ff0067"
+    },
+    ZM: {
+      name: "Zambia"
+    },
+    ZW: {
+      name: "Zimbabwe"
+    },
+    SO: {
+      name: "Somalia"
+    },
+    GF: {
+      name: "France"
+    },
+    FR: {
+      name: "France"
+    },
+    ES: {
+      name: "Spain"
+    },
+    AW: {
+      name: "Aruba"
+    },
+    AI: {
+      name: "Anguilla"
+    },
+    AD: {
+      name: "Andorra"
+    },
+    AG: {
+      name: "Antigua and Barbuda"
+    },
+    BS: {
+      name: "Bahamas"
+    },
+    BM: {
+      name: "Bermuda"
+    },
+    BB: {
+      name: "Barbados"
+    },
+    KM: {
+      name: "Comoros"
+    },
+    CV: {
+      name: "Cape Verde"
+    },
+    KY: {
+      name: "Cayman Islands"
+    },
+    DM: {
+      name: "Dominica"
+    },
+    FK: {
+      name: "Falkland Islands"
+    },
+    FO: {
+      name: "Faeroe Islands"
+    },
+    GD: {
+      name: "Grenada"
+    },
+    HK: {
+      name: "Hong Kong",
+      color: "#ff0067"
+    },
+    KN: {
+      name: "Saint Kitts and Nevis"
+    },
+    LC: {
+      name: "Saint Lucia"
+    },
+    LI: {
+      name: "Liechtenstein"
+    },
+    MF: {
+      name: "Saint Martin (French)"
+    },
+    MV: {
+      name: "Maldives"
+    },
+    MT: {
+      name: "Malta"
+    },
+    MS: {
+      name: "Montserrat"
+    },
+    MU: {
+      name: "Mauritius"
+    },
+    NC: {
+      name: "New Caledonia"
+    },
+    NR: {
+      name: "Nauru"
+    },
+    PN: {
+      name: "Pitcairn Islands"
+    },
+    PR: {
+      name: "Puerto Rico"
+    },
+    PF: {
+      name: "French Polynesia"
+    },
+    SG: {
+      name: "Singapore",
+      color: "#ff0067"
+    },
+    SB: {
+      name: "Solomon Islands"
+    },
+    ST: {
+      name: "São Tomé and Principe"
+    },
+    SX: {
+      name: "Saint Martin (Dutch)"
+    },
+    SC: {
+      name: "Seychelles"
+    },
+    TC: {
+      name: "Turks and Caicos Islands"
+    },
+    TO: {
+      name: "Tonga"
+    },
+    TT: {
+      name: "Trinidad and Tobago"
+    },
+    VC: {
+      name: "Saint Vincent and the Grenadines"
+    },
+    VG: {
+      name: "British Virgin Islands"
+    },
+    VI: {
+      name: "United States Virgin Islands"
+    },
+    CY: {
+      name: "Cyprus",
+      inactive: "yes"
+    },
+    RE: {
+      name: "Reunion (France)",
+      inactive: "yes"
+    },
+    YT: {
+      name: "Mayotte (France)",
+      inactive: "yes"
+    },
+    MQ: {
+      name: "Martinique (France)",
+      inactive: "yes"
+    },
+    GP: {
+      name: "Guadeloupe (France)",
+      inactive: "yes"
+    },
+    CW: {
+      name: "Curaco (Netherlands)",
+      inactive: "yes"
+    },
+    IC: {
+      name: "Canary Islands (Spain)",
+      inactive: "yes"
+    }
+  },
+  locations: {
+    "0": {
+      lat: 51.506,
+      lng: -0.127,
+      name: "영국"
+    },
+    "1": {
+      lat: 52.516,
+      lng: 13.377,
+      name: "독일"
+    },
+    "2": {
+      lat: 3.148,
+      lng: 101.694,
+      name: "말레이시아"
+    },
+    "3": {
+      lat: 24.469,
+      lng: 54.372,
+      name: "아랍에미리트"
+    },
+    "4": {
+      lat: -33.919,
+      lng: 18.422,
+      name: "남아프리카 공화국"
+    },
+    "5": {
+      lat: -1.284,
+      lng: 36.824,
+      name: "케냐"
+    },
+    "6": {
+      lat: 28.631,
+      lng: 77.217,
+      name: "인도"
+    },
+    "7": {
+      lat: 33.708,
+      lng: 73.07,
+      name: "파키스탄"
+    },
+    "8": {
+      lat: 22.278,
+      lng: 114.16,
+      name: "홍콩"
+    },
+    "9": {
+      lat: 35.684,
+      lng: 139.809,
+      name: "일본"
+    },
+    "10": {
+      lat: 1.29,
+      lng: 103.852,
+      name: "싱가포르"
+    },
+    "11": {
+      lat: -6.171,
+      lng: 106.826,
+      name: "인도네시아"
+    },
+    "12": {
+      lat: -35.307,
+      lng: 149.127,
+      name: "호주"
+    },
+    "13": {
+      lat: 45.422,
+      lng: -75.691,
+      name: "캐나다"
+    },
+    "14": {
+      lat: 19.432,
+      lng: -99.133,
+      name: "멕시코"
+    },
+    "15": {
+      lat: -15.778,
+      lng: -47.929,
+      name: "브라질"
+    },
+    "16": {
+      lat: 55.757,
+      lng: 37.615,
+      name: "러시아"
+    },
+    "17": {
+      lat: 60.171,
+      lng: 24.933,
+      name: "핀란드"
+    },
+    "18": {
+      lat: -41.289,
+      lng: 174.777,
+      name: "뉴질랜드"
+    },
+    "19": {
+      lat: 38.89,
+      lng: -77.032,
+      name: "미국"
+    }
+  },
+  labels: {},
+  regions: {}
+};
